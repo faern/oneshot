e5cdb553834ed3b51a1eb8f435515ace82b7a7ab
Linus Färnstrand <faern@faern.net>
2020-04-22 18:12:37 +0200

    Add initial project metadata

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! Oneshot spsc channel working both in and between sync and async environments.
+
+#![deny(unsafe_code)]
+#![deny(rust_2018_idioms)]
+
+pub fn channel() -> (Sender, Receiver) {
+    todo!();
+}
+
+pub struct Sender;
+
+pub struct Receiver;
