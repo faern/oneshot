1aaac6e0bf9bb1ac1f78f3c36bf4392bca5ef514
Linus Färnstrand <faern@faern.net>
2020-05-05 06:34:06 +0200

    Improve/restructure benchmarks

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,46 +5,80 @@ use std::time::{Duration, Instant};
 criterion_group!(benches, bench);
 criterion_main!(benches);
 
+macro_rules! bench_send_and_recv {
+    ($c:expr, $($type:ty => $value:expr);+) => {
+        // Sanity check that all $values are of $type.
+        $(let _: $type = $value;)*
+        $(bench_create_channel::<$type>($c, stringify!($type));)*
+        $(bench_create_and_send($c, stringify!($type), || $value);)*
+        $(bench_create_and_send_on_closed($c, stringify!($type), || $value);)*
+        $(bench_create_send_and_recv($c, stringify!($type), || $value);)*
+        $(bench_create_send_and_recv_ref($c, stringify!($type), || $value);)*
+    };
+}
+
 fn bench(c: &mut Criterion) {
-    bench_send_recv(c, "zst", || ());
-    bench_send_recv(c, "1byte", || 9u8);
-    bench_send_recv(c, "4096bytes", || [99u8; 4096]);
+    bench_send_and_recv!(c,
+        () => ();
+        u8 => 7u8;
+        usize => 9876usize;
+        u128 => 1234567u128;
+        [u8; 64] => [0b10101010u8; 64];
+        [u8; 4096] => [0b10101010u8; 4096]
+    );
+
     bench_try_recv(c);
     bench_recv_deadline_now(c);
     bench_recv_timeout_zero(c);
 }
 
-fn bench_send_recv<T>(c: &mut Criterion, value_name: &str, value: impl Fn() -> T) {
-    c.bench_function(&format!("create_channel_{}", value_name), |b| {
+fn bench_create_channel<T>(c: &mut Criterion, value_name: &str) {
+    c.bench_function(&format!("create_channel<{}>", value_name), |b| {
         b.iter(oneshot::channel::<T>)
     });
-    c.bench_function(&format!("create_and_send_{}", value_name), |b| {
+}
+
+fn bench_create_and_send<T>(c: &mut Criterion, value_name: &str, value_fn: impl Fn() -> T) {
+    c.bench_function(&format!("create_and_send<{}>", value_name), |b| {
         b.iter(|| {
             let (sender, _receiver) = oneshot::channel();
-            sender.send(black_box(value())).unwrap()
+            sender.send(black_box(value_fn())).unwrap()
         });
     });
-    c.bench_function(
-        &format!("create_and_send_{}_on_closed_channel", value_name),
-        |b| {
-            b.iter(|| {
-                let (sender, receiver) = oneshot::channel();
-                mem::drop(receiver);
-                sender.send(black_box(value())).unwrap_err()
-            });
-        },
-    );
-    c.bench_function(&format!("create_send_and_recv_{}", value_name), |b| {
+}
+
+fn bench_create_and_send_on_closed<T>(
+    c: &mut Criterion,
+    value_name: &str,
+    value_fn: impl Fn() -> T,
+) {
+    c.bench_function(&format!("create_and_send_on_closed<{}>", value_name), |b| {
         b.iter(|| {
             let (sender, receiver) = oneshot::channel();
-            sender.send(black_box(value())).unwrap();
+            mem::drop(receiver);
+            sender.send(black_box(value_fn())).unwrap_err()
+        });
+    });
+}
+
+fn bench_create_send_and_recv<T>(c: &mut Criterion, value_name: &str, value_fn: impl Fn() -> T) {
+    c.bench_function(&format!("create_send_and_recv<{}>", value_name), |b| {
+        b.iter(|| {
+            let (sender, receiver) = oneshot::channel();
+            sender.send(black_box(value_fn())).unwrap();
             receiver.recv().unwrap()
         });
     });
-    c.bench_function(&format!("create_send_and_recv_ref_{}", value_name), |b| {
+}
+fn bench_create_send_and_recv_ref<T>(
+    c: &mut Criterion,
+    value_name: &str,
+    value_fn: impl Fn() -> T,
+) {
+    c.bench_function(&format!("create_send_and_recv_ref<{}>", value_name), |b| {
         b.iter(|| {
             let (sender, receiver) = oneshot::channel();
-            sender.send(black_box(value())).unwrap();
+            sender.send(black_box(value_fn())).unwrap();
             receiver.recv_ref().unwrap()
         });
     });
@@ -52,9 +86,9 @@ fn bench_send_recv<T>(c: &mut Criterion, value_name: &str, value: impl Fn() -> T
 
 fn bench_try_recv(c: &mut Criterion) {
     let (sender, receiver) = oneshot::channel::<u128>();
-    c.bench_function("try_recv", |b| b.iter(|| receiver.try_recv()));
+    c.bench_function("try_recv_empty", |b| b.iter(|| receiver.try_recv()));
     mem::drop(sender);
-    c.bench_function("try_recv_closed", |b| b.iter(|| receiver.try_recv()));
+    c.bench_function("try_recv_empty_closed", |b| b.iter(|| receiver.try_recv()));
 }
 
 fn bench_recv_deadline_now(c: &mut Criterion) {
