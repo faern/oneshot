639584d5884e4af9b5c03c3bba22df3e97d97c11
Wuelle <simon.wuelker@arcor.de>
2024-05-08 23:45:21 +0200

    remove old ReceiverWaker

--- src/lib.rs
+++ src/lib.rs
@@ -1227,53 +1227,6 @@ where
     }
 }
 
-enum ReceiverWaker {
-    /// The receiver is waiting synchronously. Its thread is parked.
-    #[cfg(feature = "std")]
-    Thread(thread::Thread),
-    /// The receiver is waiting asynchronously. Its task can be woken up with this `Waker`.
-    #[cfg(feature = "async")]
-    Task(task::Waker),
-    /// A little hack to not make this enum an uninhibitable type when no features are enabled.
-    #[cfg(not(any(feature = "async", feature = "std")))]
-    _Uninhabited,
-}
-
-impl ReceiverWaker {
-    #[cfg(feature = "std")]
-    pub fn current_thread() -> Self {
-        Self::Thread(thread::current())
-    }
-
-    #[cfg(feature = "async")]
-    pub fn task_waker(cx: &task::Context<'_>) -> Self {
-        Self::Task(cx.waker().clone())
-    }
-
-    pub fn unpark(self) {
-        match self {
-            #[cfg(feature = "std")]
-            ReceiverWaker::Thread(thread) => thread.unpark(),
-            #[cfg(feature = "async")]
-            ReceiverWaker::Task(waker) => waker.wake(),
-            #[cfg(not(any(feature = "async", feature = "std")))]
-            ReceiverWaker::_Uninhabited => unreachable!(),
-        }
-    }
-}
-
-#[cfg(not(loom))]
-#[test]
-fn receiver_waker_size() {
-    let expected: usize = match (cfg!(feature = "std"), cfg!(feature = "async")) {
-        (false, false) => 0,
-        (false, true) => 16,
-        (true, false) => 8,
-        (true, true) => 16,
-    };
-    assert_eq!(mem::size_of::<ReceiverWaker>(), expected);
-}
-
 #[cfg(all(feature = "std", feature = "async"))]
 const RECEIVER_USED_SYNC_AND_ASYNC_ERROR: &str =
     "Invalid to call a blocking receive method on oneshot::Receiver after it has been polled";
